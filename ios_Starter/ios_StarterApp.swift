598990cbea132a71af31ce359ba536a7dbe34285
Ria Patel <ria2004.patel@gmail.com>
2022-09-29 12:12:33 -0400

    Initial Commit

---
+++ ios_Starter/ios_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_StarterApp.swift
+//  ios_Starter
+//
+//  Created by Ria Patel on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
